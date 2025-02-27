a2ee8a930a3ac358313afd3bfb0f8376ba8c9113
Denys Chornokon <denyschornokon3@gmail.com>
2025-02-27 17:39:53 +0200

    Connections #1

--- schedule_2/Data/ApplicationDbContext.cs
+++ schedule_2/Data/ApplicationDbContext.cs
@@ -8,6 +8,11 @@ namespace schedule_2.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+
         }
+        // DbSet'и для майбутніх моделей
+        //public DbSet<Classroom> Classrooms { get; set; }
+        //public DbSet<Course> Courses { get; set; }
+        //public DbSet<Group> Groups { get; set; }
     }
 }
--- schedule_2/obj/Release/net8.0/schedule_2.AssemblyInfo.cs
+++ schedule_2/obj/Release/net8.0/schedule_2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: Microsoft.AspNetCore.Identity.UI.UIFrameworkAttribute("Bootstrap5")]
 [assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("aspnet-schedule_2-6265896e-6cae-44dd-a338-5c3f8c30659e")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("schedule_2")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5b060d3aba19863532be976ac84cbb45adbbf0b")]
 [assembly: System.Reflection.AssemblyProductAttribute("schedule_2")]
