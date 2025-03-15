a4961cd793abb11e521f766da7a555ffef1d13cd
Denys Chornokon <denyschornokon3@gmail.com>
2025-03-15 15:13:54 +0200

    admin page #1

---
+++ schedule_2/wwwroot/js/admin.js
@@ -0,0 +1,13 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    document.querySelectorAll(".calendar-cell").forEach(cell => {
+        cell.addEventListener("click", function () {
+            let title = prompt("Enter event name:");
+            if (title) {
+                let eventDiv = document.createElement("div");
+                eventDiv.classList.add("event");
+                eventDiv.innerText = title;
+                cell.appendChild(eventDiv);
+            }
+        });
+    });
+});
--- schedule_2/wwwroot/js/site.js
+++ schedule_2/wwwroot/js/site.js
@@ -2,3 +2,8 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+document.getElementById("openEditModal").addEventListener("click", function () {
+    let modal = new bootstrap.Modal(document.getElementById("editModal"));
+    modal.show();
+});
