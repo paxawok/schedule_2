082f028532ace499ab4bbf1fbfc4d35ca1bec278
paxawok <yurcherevach@gmail.com>
2025-03-17 04:47:09 +0200

    better navigation

--- schedule_2/obj/Debug/net8.0/schedule_2.AssemblyInfo.cs
+++ schedule_2/obj/Debug/net8.0/schedule_2.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("schedule_2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7bedc04450645fce9275538378b09a3ccd95ecf1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65a124e0d2a3240f51e50488dcef0d9f7538bc07")]
 [assembly: System.Reflection.AssemblyProductAttribute("schedule_2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("schedule_2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
