546db948119747c15a1f49326999db9f2f74e3cc
Denys Chornokon <denyschornokon3@gmail.com>
2025-04-05 21:23:06 +0300

    home page fixed

--- schedule_2/obj/Debug/net8.0/schedule_2.AssemblyInfo.cs
+++ schedule_2/obj/Debug/net8.0/schedule_2.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("schedule_2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75aa8129e9fcde0767f8b6a92234f53f65f4f809")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+345ab02c4a2d9404900dc3a515c3768bf812991e")]
 [assembly: System.Reflection.AssemblyProductAttribute("schedule_2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("schedule_2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
