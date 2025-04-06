c1cf9173bc61dc661dcf1c17b5c0d8f6173bd677
paxawok <yurcherevach@gmail.com>
2025-04-06 23:27:07 +0300

    project with front

--- schedule_2/obj/Debug/net8.0/schedule_2.AssemblyInfo.cs
+++ schedule_2/obj/Debug/net8.0/schedule_2.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("schedule_2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86c1f77bdc37281cf3d574aae1fbaf75e21bdaa9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d066066ec9c76c3a81409e43e096e17ead4728d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("schedule_2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("schedule_2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
